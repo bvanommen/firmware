1abc536c679d623043590d64ba3e053218fac11b
Stefan Krastanov <stefan@krastanov.org>
2026-01-22 07:55:47 -0500

    updating the sketch behavior for the new tap detection and smoothly transitioning color after tap

--- examples/Tap_to_Measure/Tap_to_Measure.ino
+++ examples/Tap_to_Measure/Tap_to_Measure.ino
@@ -12,6 +12,7 @@ BlochVector current_state(90, 0);
 uint32_t white = color(255, 255, 255);
 uint32_t red = color(255, 0, 0);
 uint32_t blue = color(0, 0, 255);
+
 // ## Setup
 //
 // The setup function is called once when the Qbead is powered on and it is used to initialize the Qbead and set up the game.
@@ -27,46 +28,44 @@ void setup() {
 // The loop function is called repeatedly until the Qbead is powered off.
 // It is used to read the IMU and update the current state of the game.
 void loop() {
-  // Read the IMU and compute the direction of gravity and whether a tap has been detected.
+  static long last_tap = 0;
+  static uint32_t tap_color = white;
 
   // Clear the display.
   bead.clear();
 
-  bead.setBloch_deg_smooth(current_state, white);
-
-  // Show the result.
-  bead.show();
-
   if (bead.wasTapped()){
-    bead.clear();
-    BlochVector acc_vector(bead.xWhenTapped, bead.yWhenTapped, bead.zWhenTapped);
+    last_tap = millis();
+    BlochVector acc_vector(bead.x_whentapped, bead.y_whentapped, bead.z_whentapped);
 
     float probability = pow(innerProductAbs(current_state, acc_vector),2);
     float threshold = random(0, 100)/100.0f;
-    float identity_threshold = 1;
+    float identity_threshold = 0.9;
     Serial.print("probability: ");
     Serial.println(probability);
     Serial.print("threshold: ");
     Serial.println(threshold);
     if (probability > identity_threshold) {
-      bead.setBloch_deg_smooth(current_state, red);
+      tap_color = red;
       Serial.println("red identity");
     } else if (probability < 1 - identity_threshold) {
-      bead.setBloch_deg_smooth(current_state, blue);
+      tap_color = blue;
       Serial.println("blue identity");
     } else if (probability > threshold) {
       current_state = acc_vector;
-      bead.setBloch_deg_smooth(current_state, red);
+      tap_color = red;
       Serial.println("red random");
     } else {
       current_state = -acc_vector;
-      bead.setBloch_deg_smooth(current_state, blue);
+      tap_color = blue;
       Serial.println("blue random");
     }
-    bead.show();
-    delay(3000);
   }
+
+  float delta = max(min(millis() - last_tap, 2000), 0) / 2000.0;
+  uint32_t color = addColor(scaleColor(1-delta, tap_color), scaleColor(delta, white));
+  bead.setBloch_deg(current_state, color);
+
+  // Show the result.
+  bead.show();
 }
-
-// ToDo: clip to nearest pixel on qbead
-
